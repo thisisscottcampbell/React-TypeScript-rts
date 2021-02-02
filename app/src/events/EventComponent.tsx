ef1c148ef67562a6e78c2aa0f28ef66d5a70fe71
Scott Campbell <stc9486@gmail.com>
2021-02-01 22:06:57 -0500

    change event

--- app/src/events/EventComponent.tsx
+++ app/src/events/EventComponent.tsx
@@ -2,9 +2,15 @@ const EventComponent: React.FC = () => {
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
 		console.log(event.target.value);
 
+	const onDragStart = (event: React.DragEvent<HTMLDivElement>) =>
+		console.log('i am being dragged');
+
 	return (
 		<div>
 			<input onChange={handleChange} />
+			<div draggable onDragStart={onDragStart}>
+				Drag Me!
+			</div>
 		</div>
 	);
 };
