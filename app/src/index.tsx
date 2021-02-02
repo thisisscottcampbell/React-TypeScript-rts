f1dcbb6c0d4912f6b62afcfca040ae10671098b5
Scott Campbell <stc9486@gmail.com>
2021-02-01 21:28:37 -0500

    guest list state type set

---
+++ app/src/index.tsx
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom';
+import GuestList from '../src/state/GuestList';
+
+ReactDOM.render(<GuestList />, document.querySelector('#root'));
---
+++ app/src/state/GuestList.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const GuestList: React.FC = () => {
+	const [name, setName] = useState('');
+	const [guests, setGuests] = useState<string[]>([]);
+
+	const handleClick = () => {
+		setGuests([...guests, name]);
+		setName('');
+	};
+	return (
+		<div>
+			<h3>Guest List</h3>
+			{guests.map((guest) => (
+				<li>{guest}</li>
+			))}
+			<input value={name} onChange={(e) => setName(e.target.value)} />
+			<button onClick={handleClick}>Add Guest</button>
+		</div>
+	);
+};
+
+export default GuestList;
